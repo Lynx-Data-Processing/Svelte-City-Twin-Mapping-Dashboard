04cc89724d19062c8d6ae290a4b8273ebe8cf166
canaleal <alexcanales766@gmail.com>
2023-02-15 13:29:38 -0500

    Created esri map layers function

--- src/components/map/HighResMap.svelte
+++ src/components/map/HighResMap.svelte
@@ -1,8 +1,29 @@
 <script>
 	import { onMount } from 'svelte';
-	import { esriAPIConfig, esriMapConfig } from '../../utils/highResMap/highResMap-utils';
+	import { GeojsonEnum } from '../../types/enums';
+	import { axiosCacheGetUtility } from '../../utils/fetch-data';
+	import {
+		addFeatureLayer,
+		addTileLayer,
+		createBaseMapToggle,
+		createMapAndSceneView,
+		createMapWidgets,
+		esriAPIConfig
+	} from '../../utils/highResMap/highResMap-utils';
 
-	onMount(async () => {
+	import {
+		PUBLIC_MAPBOX_KEY,
+		PUBLIC_OPEN_DATA_KINGSTON_CITY_ZONES_URL,
+		PUBLIC_PLANNING_LINE_URL,
+		PUBLIC_PLANNING_POINT_URL,
+		PUBLIC_SIDEWALK_POLYGON_URL,
+		PUBLIC_TREES_URL
+	} from '$env/static/public';
+
+	let view;
+	let map;
+
+	const createESRIMap = async () => {
 		// Load the modules for the ArcGIS API for JavaScript
 		// They cannot be loaded using ES modules because they are in common JS format and conflict with VITE
 		require([
@@ -18,7 +39,9 @@
 			'esri/layers/TileLayer',
 			'esri/views/SceneView',
 			'esri/layers/FeatureLayer',
-			'esri/layers/ElevationLayer'
+			'esri/layers/ElevationLayer',
+			'esri/widgets/BasemapToggle',
+			'esri/widgets/BasemapGallery'
 		], (
 			esriConfig,
 			Map,
@@ -32,27 +55,31 @@
 			TileLayer,
 			SceneView,
 			FeatureLayer,
-			ElevationLayer
+			ElevationLayer,
+			BasemapToggle,
+			BasemapGallery
 		) => {
 			//* Config esri API key
 			esriAPIConfig(esriConfig);
-
-			//* Create esri map
-			esriMapConfig(
-				Map,
-				MapView,
-				Home,
-				ScaleBar,
-				LayerList,
-				Legend,
-				Expand,
-				Compass,
-				TileLayer,
-				SceneView,
+			[map, view] = createMapAndSceneView(Map, SceneView, ElevationLayer, TileLayer);
+			createMapWidgets(view, Home, ScaleBar, LayerList, Legend, Expand, Compass);
+			createBaseMapToggle(view, BasemapToggle, BasemapGallery);
+			addFeatureLayer(
+				map,
 				FeatureLayer,
-				ElevationLayer
+				'https://services1.arcgis.com/5GRYvurYYUwAecLQ/arcgis/rest/services/Waste_Collection_Area/FeatureServer'
+			);
+
+			addFeatureLayer(
+				map,
+				FeatureLayer,
+				'https://services1.arcgis.com/5GRYvurYYUwAecLQ/ArcGIS/rest/services/Trees1/FeatureServer'
 			);
 		});
+	};
+
+	onMount(async () => {
+		createESRIMap();
 	});
 </script>
 
