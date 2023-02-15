d6d9f7b456b2811e6a2d236bd3a0e2fe02fc64c3
Alex Carvalho <alexander_sck@live.ca>
2023-02-15 18:04:38 -0500

    feat: create date-time formatting util

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -28,6 +28,13 @@ export enum FontAwesomeIconGivenGeojsonEnum {
     GeometryCollection = 'fa-solid fa-square',
     Feature = 'fa-solid fa-map',
     FeatureCollection = 'fa-solid fa-square',
+}
 
-
+export enum TimeStampFormatEnum {
+    YYYY_MM_DD_hh_mm_ss = 'yyyy-mm-dd hh:mm:ss',
+    YY_MM_DD_hh_mm_ss = 'yy-mm-dd hh:mm:ss',
+    YYYY_MM_DD_hh_mm = 'yyyy-mm-dd hh:mm',
+    YY_MM_DD_hh_mm = 'yy-mm-dd hh:mm',
+    DateFull = 'Day Month Date Year hh:mm:ss UTC',
+    DateSimple = 'Month Date Year hh:mm',
 }
