56bb3299ed6563b7c3290aee174f09fa4bca6c0f
amir002001 <a_amirhossein@icloud.com>
2023-02-27 08:45:56 -0500

    fix: adds h1 tag to header

--- src/widgets/Navbar.svelte
+++ src/widgets/Navbar.svelte
@@ -1,5 +1,5 @@
 <navbar class="shadow-md py-2 px-8  flex justify-between align-middle text-center">
 	<div class="flex flex-row ">
-		<p class="font-bold text-2xl"><i class="fa-solid fa-location-arrow" /> LYNX</p>
+		<h1 class="font-bold text-2xl uppercase"><i class="fa-solid fa-location-arrow mr-1" />lynx</h1>
 	</div>
 </navbar>
