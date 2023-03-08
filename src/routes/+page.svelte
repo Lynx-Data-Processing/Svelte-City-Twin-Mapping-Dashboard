0a68d12b798d6b346bca2816b16d30c4baec81a0
Alex Carvalho <alexander_sck@live.ca>
2023-03-08 16:43:00 -0500

    refactor: update login ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import '../styles/style.css';
 	import { Auth } from '@supabase/auth-ui-svelte';
 	import { ThemeSupa } from '@supabase/auth-ui-shared';
+	import login from '$lib/images/login.png';
+
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -10,14 +13,21 @@
 	<title>Login</title>
 </svelte:head>
 
-<div class="row flex-center flex">
-	<div class="col-6 form-widget">
-		<Auth
-			supabaseClient={data.supabase}
-			view="magic_link"
-			redirectTo={`${data.url}/login?redirect=/lynx-city-twin`}
-			showLinks={false}
-			appearance={{ theme: ThemeSupa, style: { input: 'color: #fff' } }}
-		/>
+<main>
+	<div class="w-screen h-screen flex flex-col items-center justify-center">
+		<div class="z-10 flex flex-col items-center">
+			<div class="flex flex-col">
+				<h1 class="font-bold text-5xl uppercase"><i class="fa-solid fa-location-arrow mr-1"/>lynx</h1>
+				<h2 class="font-bold">City Mapping Dashboard</h2>
+			</div>
+
+			<Auth
+				supabaseClient={data.supabase}
+				view="magic_link"
+				redirectTo={`${data.url}/login?redirect=/lynx-city-twin`}
+				showLinks={false}
+				appearance={{ theme: ThemeSupa, style: { input: 'color: #000' } }}
+			/>
+		</div>
 	</div>
-</div>
+</main>
