aaef6cf2b9e78f8ae930f916850ef5983f06e71d
Alex Carvalho <alexander_sck@live.ca>
2023-03-09 10:19:15 -0500

    refactor: update login ui styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,20 +9,23 @@
 </script>
 
 <main>
-	<div class="w-screen h-screen flex flex-col items-center justify-center">
-		<div class="z-10 flex flex-col items-center">
-			<div class="flex flex-col">
-				<h1 class="font-bold text-5xl uppercase"><i class="fa-solid fa-location-arrow mr-1"/>lynx</h1>
-				<h2 class="font-bold">City Mapping Dashboard</h2>
-			</div>
-
+	<div class="login-background">
+		<div class="login-info text-white">
+			<h1 class="font-sans text-7xl uppercase "><i class="fa-solid fa-location-arrow mr-5"/>lynx</h1>
+			<p class="mt-12 text-lg">Cloud-based software to create and share interactive web maps.</p>
+		</div>
+		<div class="login-info">
+			<a href="/" class="mb-auto text-right text-gray-800 hover:underline">Need Help?</a>
+			<h1 class="font-sans text-4xl">Sign Up</h1>
 			<Auth
 				supabaseClient={data.supabase}
 				view="magic_link"
 				redirectTo={`${data.url}/login?redirect=/lynx-city-twin`}
 				showLinks={false}
-				appearance={{ theme: ThemeSupa, style: { input: 'color: #000' } }}
+				appearance={{ theme: ThemeSupa, style: { input: 'color: #000', label: 'display: none' } }}
 			/>
+			<a href="/" class="mb-auto text-center text-gray-800 hover:underline">Why a link? Learn More</a>
 		</div>
+		<div class="bottom-block"/>
 	</div>
 </main>
