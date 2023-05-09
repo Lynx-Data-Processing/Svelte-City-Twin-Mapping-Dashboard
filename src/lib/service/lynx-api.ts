5a2e319690fabdf557503b17f88492d2d8fbcde3
Alex Carvalho <alexander_sck@live.ca>
2023-05-08 20:38:52 -0400

    feat: create device info endpoint function

--- src/lib/service/lynx-api.ts
+++ src/lib/service/lynx-api.ts
@@ -1,34 +1,61 @@
 import axios from "axios";
+import { PUBLIC_NODE_BACKEND_URL } from '$env/static/public';
 
 // root smarterAI enpoint
 
 // get all devices from smarterAI
 export const getDevices = async () => {
-  
+
     const config = {
-      method: 'get',
-      url: `${NODE_BACKEND_URL}/devices`,
-      headers: {
-        'Content-Type': 'application/json',
-      },
+        method: 'get',
+        url: `${PUBLIC_NODE_BACKEND_URL}/devices`,
+        headers: {
+            'Content-Type': 'application/json',
+        },
     };
   
-  
     try {
-      const promise = await axios(config);
-      return promise;
+        const promise = await axios(config);
+        return promise;
     } catch (error: any) {
-      if (error.response) {
-        return error.response.status;
-      } if (error.request) {
-        return error.request;
-      }
-      return error.message;
+        if (error.response) {
+            return error.response.status;
+        } 
+        if (error.request) {
+            return error.request;
+        }
+        return error.message;
     }
-  };
+};
 
+// Get information from a specific device
+export const getInfo = async (deviceId: string) => {
 
-// info endpoint
+    const params = new URLSearchParams({
+        endpointId: deviceId,
+    });
+    
+    const config = {
+        method: 'get',
+        url: `${PUBLIC_NODE_BACKEND_URL}/info?${params.toString()}`,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    
+    try {
+        const promise = await axios(config);
+        return promise;
+    } catch (error: any) {
+        if (error.response) {
+            return error.response.status;
+        } 
+        if (error.request) {
+            return error.request;
+        }
+        return error.message;
+    }
+}
 
 // events endpoint
 
