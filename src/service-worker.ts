b55063b9cb2684f0b25bd69c64ea7571709f1ed0
Alex Carvalho <alexander_sck@live.ca>
2023-08-09 13:02:41 -0400

    feat: create cache and assets

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -2,3 +2,15 @@
 // TODO: 
 // - Load Google Map / Map Data
 // - Load Kingston Data
+// - Cache Static Data
+
+/// <reference types="@sveltejs/kit" />
+import { build, files, version } from '$service-worker';
+
+const CACHE = `cache-${version}`;
+
+const ASSETS = [
+    ...build,
+    ...files,
+];
+
