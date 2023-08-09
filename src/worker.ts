53a5aaad64bd6d91dd62d17052163f963920b9ff
Alex Carvalho <alexander_sck@live.ca>
2023-08-09 14:52:20 -0400

    feat: config for map load and kingston data

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,54 @@
-// Web worker for loading Google map data and Kingston data
+// Web worker for loading Google maps data and Kingston data on a separate thread
 
 // TODO:
-// - Load Google Map / Map Data
-// - Load Kingston Data
+// - Load Google maps data
+// - Load Kingston data
+
+import type { 
+    IMapDetailsType,
+    ILayerListElementType,
+} from '$lib/types/mapTypes';
+import type {  IEventGoogleDataType } from '$lib/types/eventTypes';
+import { getKingstonMapData } from '$lib/utils/geojson/kingston-geojson-util';
+import {
+    addLayerElementToLayerList,
+    addLayerToGoogleMap,
+    toggleGoogleMapLayerVisibility
+} from '$lib/utils/geojson/google-map-utils';
+
+
+
+let selectedEvent: IEventGoogleDataType | null = null;
+const updateSelectedEvent = (googleMapEvent: IEventGoogleDataType) => {
+    selectedEvent = googleMapEvent;
+};
+
+const initializeMap = async () => {
+    // Map details
+	let layerList: ILayerListElementType[] = [];
+    let mapDetails: IMapDetailsType = {
+		mapTypeId: 'roadmap',
+		center: { lng: -76.491143, lat: 44.231689 },
+		zoom: 17,
+		tilt: 60,
+		heading: -17.6
+	};
+
+    // Create map
+    let mapDiv = new HTMLDivElement();
+    let map = new google.maps.Map(mapDiv, mapDetails);
+
+    // Get kingston data
+    let tempKingstonLayers = await getKingstonMapData();
+	if (!tempKingstonLayers || !tempKingstonLayers.length) return;
+
+    // Load kingston data onto map
+    for (let i = 0, len = tempKingstonLayers.length; i < len; i++) {
+        layerList = addLayerElementToLayerList(layerList, tempKingstonLayers[i]);
+        map = addLayerToGoogleMap(map, tempKingstonLayers[i], updateSelectedEvent);
+        map = toggleGoogleMapLayerVisibility(map, tempKingstonLayers[i]);
+    }
+
+    // Return map
+    return map;
+}
