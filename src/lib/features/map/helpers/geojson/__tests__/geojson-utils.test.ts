1c84f0283c38286a61c3ca455dfa5826e6a43f84
Alex Canales <alexcanales766@gmail.com>
2023-09-10 01:39:59 -0400

    Added geojson coverage

--- src/lib/features/map/helpers/geojson/__tests__/geojson-utils.test.ts
+++ src/lib/features/map/helpers/geojson/__tests__/geojson-utils.test.ts
@@ -1,42 +1,109 @@
 // tests/geojson.spec.ts
-import { test, expect } from 'vitest'
-import { POINT, LINE_STRING, MULTI_POINT, POLYGON, MULTI_LINE_STRING, MULTI_POLYGON } from "../../../constants/geojson"; // Update the import based on your actual file path
-import { convertToGeojson, addAdditionalStylingToGeojson, convertGeoJSONToLatLng } from "../geojson-utils"; // Update the import based on your actual file path
-import type { IGeojsonCollection, IGeojsonFeature } from '$lib/features/map/types';
+import { test, expect } from 'vitest';
+import { POINT, LINE_STRING, MULTI_POINT, POLYGON, MULTI_LINE_STRING, MULTI_POLYGON } from "../../../constants/geojson";
+import { addAdditionalStylingToGeojson, convertGeoJSONToLatLng } from "../geojson-utils"; // Update the import based on your actual file path
+import type { IGeojsonCollection, IGeojsonFeature } from '$lib/features/map/types/geojsonTypes';
 
-test('convertToGeojson converts data to GeoJSON format', () => {
-  const data = [
-    { id: 1, location: { lat: 12, lng: 34 } },
-    { id: 2, location: { lat: 56, lng: 78 } }
-  ];
-  const geojson = convertToGeojson(data, POINT);
-
-  expect(geojson.type).toBe('FeatureCollection');
-  expect(geojson.features.length).toBe(2);
-  expect(geojson.features[0].geometry.type).toBe(POINT);
-  expect(geojson.features[0].geometry.coordinates).toEqual([34, 12]);
-});
-
-test('addAdditionalStylingToGeojson adds styling', () => {
-  const initialGeojson: IGeojsonCollection = {
-    type: 'FeatureCollection',
-    features: [ { type: 'Feature', properties: {}, geometry: { type: POINT, coordinates: [0, 0] } } ]
-  };
-  const styledGeojson = addAdditionalStylingToGeojson(initialGeojson, 'red', true);
-
-  expect(styledGeojson.features[0].properties.color).toBe('red');
-  expect(styledGeojson.features[0].properties.hasArrows).toBe(true);
-});
-
-test('convertGeoJSONToLatLng converts a GeoJSON feature to LatLng', () => {
-  const geoJSONFeature: IGeojsonFeature = {
+test('convertGeoJSONToLatLng returns correct coordinates for POINT', () => {
+  const geoJSON: IGeojsonFeature = {
     type: 'Feature',
     properties: {},
-    geometry: {
-      type: POINT,
-      coordinates: [34, 12]
-    }
+    geometry: { type: POINT, coordinates: [34, 12] }
   };
-  const latLng = convertGeoJSONToLatLng(geoJSONFeature);
-  expect(latLng).toEqual({ lat: 12, lng: 34 });
+  expect(convertGeoJSONToLatLng(geoJSON)).toEqual({ lat: 12, lng: 34 });
+});
+
+test('convertGeoJSONToLatLng returns correct coordinates for LINE_STRING', () => {
+  const geoJSON: IGeojsonFeature = {
+    type: 'Feature',
+    properties: {},
+    geometry: { type: LINE_STRING, coordinates: [[34, 12], [56, 78]] }
+  };
+  expect(convertGeoJSONToLatLng(geoJSON)).toEqual({ lat: 12, lng: 34 });
+});
+
+test('convertGeoJSONToLatLng returns correct coordinates for MULTI_POINT', () => {
+  const geoJSON: IGeojsonFeature = {
+    type: 'Feature',
+    properties: {},
+    geometry: { type: MULTI_POINT, coordinates: [[34, 12], [56, 78]] }
+  };
+  expect(convertGeoJSONToLatLng(geoJSON)).toEqual({ lat: 12, lng: 34 });
+});
+
+test('convertGeoJSONToLatLng returns correct coordinates for POLYGON', () => {
+  const geoJSON: IGeojsonFeature = {
+    type: 'Feature',
+    properties: {},
+    geometry: { type: POLYGON, coordinates: [[[34, 12], [56, 78], [34, 12]]] }
+  };
+  expect(convertGeoJSONToLatLng(geoJSON)).toEqual({ lat: 12, lng: 34 });
+});
+
+
+
+
+
+test('addAdditionalStylingToGeojson adds default color when no color is passed', () => {
+    const initialGeojson: IGeojsonCollection = {
+      type: 'FeatureCollection',
+      features: [ { type: 'Feature', properties: {}, geometry: { type: 'Point', coordinates: [0, 0] } } ]
+    };
+    const styledGeojson = addAdditionalStylingToGeojson(initialGeojson);
+  
+    expect(styledGeojson.features[0].properties.color).toBe("#000000");
+  });
+  
+  test('addAdditionalStylingToGeojson adds specified color', () => {
+    const initialGeojson: IGeojsonCollection = {
+      type: 'FeatureCollection',
+      features: [ { type: 'Feature', properties: {}, geometry: { type: 'Point', coordinates: [0, 0] } } ]
+    };
+    const styledGeojson = addAdditionalStylingToGeojson(initialGeojson, 'red');
+  
+    expect(styledGeojson.features[0].properties.color).toBe('red');
+  });
+  
+  test('addAdditionalStylingToGeojson adds hasArrows property when specified', () => {
+    const initialGeojson: IGeojsonCollection = {
+      type: 'FeatureCollection',
+      features: [ { type: 'Feature', properties: {}, geometry: { type: 'Point', coordinates: [0, 0] } } ]
+    };
+    const styledGeojson = addAdditionalStylingToGeojson(initialGeojson, 'red', true);
+  
+    expect(styledGeojson.features[0].properties.hasArrows).toBe(true);
+  });
+  
+  test('addAdditionalStylingToGeojson does not add hasArrows property when not specified', () => {
+    const initialGeojson: IGeojsonCollection = {
+      type: 'FeatureCollection',
+      features: [ { type: 'Feature', properties: {}, geometry: { type: 'Point', coordinates: [0, 0] } } ]
+    };
+    const styledGeojson = addAdditionalStylingToGeojson(initialGeojson, 'red');
+  
+    expect(styledGeojson.features[0].properties.hasArrows).toBeUndefined();
+  });
+
+test('convertGeoJSONToLatLng returns correct coordinates for MULTI_LINE_STRING', () => {
+  const geoJSON: IGeojsonFeature = {
+    type: 'Feature',
+    properties: {},
+    geometry: { type: MULTI_LINE_STRING, coordinates: [[[34, 12], [56, 78]]] }
+  };
+  expect(convertGeoJSONToLatLng(geoJSON)).toEqual({ lat: 12, lng: 34 });
+});
+
+test('convertGeoJSONToLatLng returns correct coordinates for MULTI_POLYGON', () => {
+  const geoJSON: IGeojsonFeature = {
+    type: 'Feature',
+    properties: {},
+    geometry: { type: MULTI_POLYGON, coordinates: [[[[34, 12], [56, 78], [34, 12]]]] }
+  };
+  expect(convertGeoJSONToLatLng(geoJSON)).toEqual({ lat: 12, lng: 34 });
+});
+
+
+test('convertGeoJSONToLatLng returns {lat: 0, lng: 0} for invalid input', () => {
+  expect(convertGeoJSONToLatLng(null as any)).toEqual({ lat: 0, lng: 0 });
+  expect(convertGeoJSONToLatLng(undefined as any)).toEqual({ lat: 0, lng: 0 });
 });
--- src/lib/utils/value-type.ts
+++ src/lib/utils/value-type.ts
@@ -1,41 +1,15 @@
 export function isImage(url: any) {
-    try {
-        return typeof url === 'string' && /\.(jpeg|jpg|gif|png)$/i.test(url);
-    } catch (e) {
-        return false;
-    }
+    return typeof url === 'string' && /\.(jpeg|jpg|gif|png)$/i.test(url);
 }
-
 export function isVideo(url: any) {
-    try {
-        return typeof url === 'string' && /\.(mp4|webm|ogg)$/i.test(url);
-    } catch (e) {
-        return false;
-    }
+    return typeof url === 'string' && /\.(mp4|webm|ogg)$/i.test(url);
 }
-
-
 export function isColor(value: any) {
-    try {
-        return typeof value === 'string' && /^#([0-9a-f]{3}){1,2}$/i.test(value);
-    } catch (e) {
-        return false;
-    }
+    return typeof value === 'string' && /^#([0-9a-f]{3}){1,2}$/i.test(value);
 }
-
 export function isNumber(value: any) {
-    try {
-        return !isNaN(parseFloat(value)) && isFinite(value);
-    } catch (e) {
-        return false;
-    }
+    return !isNaN(parseFloat(value)) && isFinite(value);
 }
-
-
 export function stringifyObject(obj: any): string {
-    try {
-        return typeof obj === 'object' ? JSON.stringify(obj) : obj;
-    } catch (e) {
-        return 'Invalid Object';
-    }
+    return typeof obj === 'object' ? JSON.stringify(obj) : obj;
 }
